1506fc9afd25b58e4133db4df98515d14c0ca975
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-06-30 19:08:34 +0800

    two click page

--- html/index.php
+++ html/index.php
@@ -4,7 +4,7 @@
 	<h3>Make sure your phone’s on 3G/4G not WiFi and hit a button! Show me the:</h3>
 
 	<a href="one-click.php" class="button">One-Click Button</a><br>
-	<a href="#" class="button">Two-Click Button</a><br>
+	<a href="two-click.php" class="button">Two-Click Button</a><br>
 
 	<a href="#" class="button2">Get a button for your cause</a>
 
--- html/two-click.php
+++ html/two-click.php
@@ -3,10 +3,14 @@
 	<p class="first-title">You can put this button almost anywhere – including Facebook posts, YouTube and emails.</p>
 	<p class="second-title">Try it with a £3 donation to the DEC Nepal Appeal now.</p>
 
-	<div class="form-container">
-		<p>INSERT PAYMENT FORM HERE</p>
+	<div class="two-click-container">
+
+		<div class="two-click-image" style="background-image: url(images/two-click-image.png);"></div>
+
+		<a href="#" class="button3">Give £3 now</a>
+
 	</div>
-	
+
 </section>
 
 <?php include('footer.php'); ?>
