7bff62e06ef2f70c16bb3bf0536910343efc2621
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-07-01 11:18:05 +0800

    contact us page

---
+++ html/contact-us.php
@@ -0,0 +1,12 @@
+<?php include('header.php'); ?>	
+
+	<h1>Contact Us</h1><br>
+	<h4>Get in touch today to find out how your cause can benefit from an OMG button...</h4>
+
+	<a href="#" class="button2">Call us on 020 7490 9999 <img src="images/phone-icon.png"></a>
+	<a href="#" class="button2">Email us <img src="images/email-icon.png"></a>
+	<a href="#" class="button2">Home <img src="images/home-icon.png"></a>
+
+</section>
+
+<?php include('footer.php'); ?>
--- html/header.php
+++ html/header.php
@@ -13,6 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" type="text/css" href="css/font.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
 <!-- responsive style -->
 
