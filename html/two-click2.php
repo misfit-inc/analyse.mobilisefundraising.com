e820837396e13323f0758641a622db21badc3f4d
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-07-01 14:44:35 +0800

    media query

---
+++ html/two-click2.php
@@ -0,0 +1,16 @@
+<?php include('header.php'); ?>	
+
+	<p class="first-title">You can put this button almost anywhere – including Facebook posts, YouTube and emails.</p>
+	<p class="second-title">Try it with a 50p donation now. We’ll double your gift & send it to the DEC Nepal Appeal.</p>
+
+	<div class="two-click-container">
+
+		<div class="two-click-image" style="background-image: url(images/two-click-image.png);">
+			<a href="two-form.php" class="button3">Give 50p now</a>
+		</div>
+
+	</div>
+
+</section>
+
+<?php include('footer.php'); ?>
