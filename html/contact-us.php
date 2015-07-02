72d96eca0a163455f8297b8cce002f957f5dcd35
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-07-02 17:07:09 +0800

    added email to email button

--- html/contact-us.php
+++ html/contact-us.php
@@ -4,7 +4,7 @@
 	<h4>Get in touch today to find out how your cause can benefit from an OMG button...</h4>
 
 	<a href="tel:020-7490-9999" class="button2">Call us on 020 7490 9999 <img src="images/phone-icon.png" class="icon"></a>
-	<a href="#" class="button2">Email us <img src="images/email-icon.png" class="icon"></a>
+	<a href="mailto:hello@openfundraising.com" class="button2">Email us <img src="images/email-icon.png" class="icon"></a>
 	<a href="index.php" class="button2 last">Home <img src="images/home-icon.png" class="icon"></a>
 
 </section>
