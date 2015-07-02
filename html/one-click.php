527323c175e82c89e806e7efa85d1513d1efd836
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-07-02 15:56:10 +0800

    change amount donation

--- html/one-click.php
+++ html/one-click.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>	
 
 	<p class="first-title">You can put this button almost anywhere – including Facebook posts, YouTube and emails.</p>
-	<p class="second-title">Try it with a £3 donation to the DEC Nepal Appeal now.</p>
+	<p class="second-title">Try it with a £0.50 donation to the DEC Nepal Appeal now.</p>
 
 	<div class="form-container">
 		<iframe src="http://fpay.co/?sid=106053&skipSuccess=true" scrolling="no" style="border: none; margin-bottom: 30px;"></iframe>
--- html/two-click.php
+++ html/two-click.php
@@ -1,12 +1,12 @@
 <?php include('header.php'); ?>	
 
 	<p class="first-title">You can put this button almost anywhere – including Facebook posts, YouTube and emails.</p>
-	<p class="second-title">Try it with a £3 donation to the DEC Nepal Appeal now.</p>
+	<p class="second-title">Try it with a £0.50 donation to the DEC Nepal Appeal now.</p>
 
 	<div class="two-click-container">
 
 		<div class="two-click-image" style="background-image: url(images/two-click-image.png);">
-			<a href="two-form.php" class="button3">Give £3 now</a>
+			<a href="two-form.php" class="button3">Give £0.50 now</a>
 		</div>
 
 	</div>
